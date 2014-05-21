2bbaec5cc8e3ce510b5d840c2e3bd781df63cc75
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-21 22:28:06 +0400

    Author: David Ghabuzian --Changed functionality of html

---
+++ HtmlCssJavaScript/JavaScripAndHtml/HomeWork/Js/person.js
@@ -0,0 +1,18 @@
+var person = {
+    firstName : "Name",
+    surName : "Surname",
+    age : 0,
+    eyeColor : "Brown",
+};
+function addPerson() {
+    person.firstName = document.getElementById("idFirstName").value;
+    person.surName = document.getElementById("idSurName").value;
+    person.eyeColor = document.getElementById("idEyeColor").value;
+    var list = document.getElementById("idAge");
+    var index = list.selectedIndex;
+    person.age = list[index].value;
+}
+function getPerson() {
+    document.getElementById("demo").innerHTML = person.firstName + " " + person.surName 
+        + " is " + person.age + " years old and has " + " " + person.eyeColor + " eyes.";
+}
