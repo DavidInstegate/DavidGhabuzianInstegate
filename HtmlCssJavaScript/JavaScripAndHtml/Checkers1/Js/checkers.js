7dc1d72ae7dcb6512e655d75eaa1258dd74c98d4
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-22 19:00:01 +0400

    auto update repository

--- HtmlCssJavaScript/JavaScripAndHtml/Checkers1/Js/checkers.js
+++ HtmlCssJavaScript/JavaScripAndHtml/Checkers1/Js/checkers.js
@@ -1,31 +1,30 @@
-var idBoard="idBoard"
-var idBoardSize="idBoardSize"
-var gameBoard{
-    color : "black";
-}
+var idBoard="idBoard";
+var idBoardSize="idBoardSize";
+var gameBoard = {
+     createGameBoard : function () {
+        alert('createGameBoard');
+        var table = document.getElementById(idBoard);
+        var boardSize = document.getElementById(idBoardSize).value;
+        for(var i = 0; i < boardSize; ++i) {
+            alert("for I");
+            var rowElement = document.createElement("tr");
+            for(var j = 0; j < boardSize; ++j) {
+                var columeElement = document.createElement("td");
+                columeElement.innerHTML ="<p>j + 1</p>";
+                rowElement.appendChild(columeElement);
+            }
+            table.appendChild(rowElement);
 
-function createGameBoard() {
-    alert("createGameBoard");
-    var table = document.getElementById(idBoard);
-    var boardSize = document.getElementById(idBoardSize);
-    for(var i =0; i < boardSize; ++i) {
-        var rowElement = document.createElement("tr");
-        for(var j = 0; j < boardSize; ++j) {
-            var columeElement = document.createElement("td");
-            columeElement = document.appendChild(document.createTextNode(j+1));
-            rowElement.appendChild(collumeElement);
         }
-        table.appendChild(rowElement);
 
     }
-
-}
+};
 function startGame() {
-    var table = document.getElementById(idBoard);
+ //   var table = document.getElementById(idBoard);
     alert("Start Game");
-    createGameBoard();
-    var row = table.rows[2];
-    var cell = row.cells[3];
-    cell.innerHTML = '<input id="Button" type="button" value="ClickHere" />';
+    gameBoard.createGameBoard();
+  //  var row = table.rows[2];
+  //  var cell = row.cells[3];
+  //  cell.innerHTML = '<input id="Button" type="button" value="ClickHere" />';
 }
 
