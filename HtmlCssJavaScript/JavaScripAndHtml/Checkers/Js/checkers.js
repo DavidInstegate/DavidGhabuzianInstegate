575c02d2bf472a21b53e63246c5e925d38ccbc6a
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-22 12:33:25 +0400

    Author:David Ghabuzian --Inserted butoon into table

--- HtmlCssJavaScript/JavaScripAndHtml/Checkers/Js/checkers.js
+++ HtmlCssJavaScript/JavaScripAndHtml/Checkers/Js/checkers.js
@@ -2,9 +2,8 @@ var idBoard="idBoard"
 function startGame() {
     var table = document.getElementById(idBoard);
     alert("Start Game");
-    var button = document.createElenemt("input");
-    button.setAttribute("type","button");
-    button.setAttribute("name","Black");
-    tabble.rows[2].cells[3].appendChild(button);
+    var row = table.rows[2];
+    var cell = row.cells[3];
+    cell.innerHTML = '<input id="Button" type="button" value="ClickHere" />';
 }
 
