e8d1cf896559408f39a666d7448856c525652226
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-22 11:00:01 +0400

    auto update repository

---
+++ HtmlCssJavaScript/JavaScripAndHtml/Checkers/Js/checkers.js
@@ -0,0 +1,8 @@
+var idBoard="idBoard"
+function startGame() {
+    alert("Start Game");
+    var table = document.getElementById(idBoard);
+    var button = document.creatElement("button");
+    tabble.rows[3].cells[3].value = button;
+}
+
