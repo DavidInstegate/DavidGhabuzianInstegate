2016877fbfb1508b619cc98ce074639388db6330
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-24 20:00:01 +0400

    auto update repository

--- HtmlCssJavaScript/JavaScripAndHtml/Checkers3/Js/checkers.js
+++ HtmlCssJavaScript/JavaScripAndHtml/Checkers3/Js/checkers.js
@@ -6,23 +6,44 @@ var greenColor = "green";
 var redColor = "red";
 
 function GameBoard(boardSize) {
+    alert("GameBoard");
     this.mBoardSize = boardSize || 8;
-    this.mGameBoard = [];
+    this.mGameBoard = new Array(boardSize);
+    this.mTable = document.getElementById(idBoard);
     for (var i = 0; i < boardSize; ++i) {
-        this.mGameBoard[i] = [];
+        this.mGameBoard[i] = new Array(boardSize);
         for (var j = 0; j < boardSize; ++j) {
             this.mGameBoard[i][j] = boardSize * i + j;
         }
     }
     this.printBoard = printBoard;
+    alert("endGameBoar");
 }
 function printBoard() {
-    var table = getElementById(idBoard);
-    
+    alert("printBoard");
+    var table = this.mTable; 
+    boardSize = this.mBoardSize;
+    for(var i = 0; i < boardSize; ++i) {
+        var rowElement = document.createElement("tr");
+        for(var j = 0; j < boardSize; ++j) {
+            var columeElement = document.createElement("td");
+            if((i+j) % 2 == 0) {
+                columeElement.style.backgroundColor = whiteColor;
+            } else {
+                columeElement.style.backgroundColor = blackColor;
+            }
+            columeElement.align = "center";
+            var id = i * this.boardSize + j;
+            columeElement.setAttribute("id","id" + id);
+        }
+        rowElement.appendChild(columeElement);
+        table.appendChild(rowElement);
+    }
+    alert("end printBoard");
 }
 function startGame() {
     alert("Start Game");
-    var boardSize = document.getElementById("idBoardSize");
+    var boardSize = document.getElementById(idBoardSize);
     var gameBoard = new GameBoard(boardSize);
     gameBoard.printBoard();
 }
