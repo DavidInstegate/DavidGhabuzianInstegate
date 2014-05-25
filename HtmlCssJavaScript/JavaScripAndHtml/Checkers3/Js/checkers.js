c73e7775f8d6a71ef6a3e285380cefc29cc2e2c2
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-25 21:00:01 +0400

    auto update repository

--- HtmlCssJavaScript/JavaScripAndHtml/Checkers3/Js/checkers.js
+++ HtmlCssJavaScript/JavaScripAndHtml/Checkers3/Js/checkers.js
@@ -5,22 +5,18 @@ var whiteColor = "white";
 var greenColor = "green";
 var redColor = "red";
 function Feild(i,j) {
-    alert("Feild");
     this.mI = i || -1;
     this.mJ = j || -1;
     this.setI = setI;
     this.setJ = setJ;
     this.getI = getI;
     this.getJ = getJ;
-    alert("end Feild");
 }
 function setI(i) {
-    alert("setI");
     this.mI = i;
 }
 
 function setJ(j) {
-    alert("setJ");
     this.mJ = j;
 }
 
@@ -33,13 +29,11 @@ function getI() {
 }
 
 function Figure(i,j,figureColor) {
-    alert("Figure");
     this.mOcupatedFeild = new Feild(i,j);
     this.mFigueColor = figureColor || redColor;
     this.getFigureColor = getFigureColor;
     this.getOcupatedFeild = getOcupatedFeild;
     this.setOcupatedFeild = setOcupatedFeild;
-    alert("end Figure");
 }
 
 function setOcupatedFeild(i,j) {
@@ -52,6 +46,7 @@ function getOcupatedFeild() {
 }
 
 function getFigureColor() {
+    alert("getFigureColor");
     return this.mFigureColor;
 }
 
@@ -60,6 +55,7 @@ function GameBoard(boardSize) {
     this.mBoardSize = boardSize || 8;
     this.mGameBoard = new Array(boardSize); 
     this.mTable = document.getElementById(idBoard);
+    this.putFigure = putFigure;
     for(var i = 0; i < boardSize; ++i) {
         this.mGameBoard[i] = new Array(boardSize);
         for (var j = 0; j < boardSize; ++j) {
@@ -68,12 +64,13 @@ function GameBoard(boardSize) {
     }
     for(var i = 0; i < 3; ++i) {
         for(var j = i % 2; j < boardSize; j += 2) {
-//            var figure = new Figure(i,j,greenColor);
+            var figure = new Figure(i,j,greenColor);
+            this.mGameBoard[i][j] = figure;
         }
     }
     this.clearTable = clearTable;
     this.printBoard = printBoard;
-    alert("endGameBoar");
+    alert("end GameBoard");
 }
 
 function clearTable(table) {
@@ -84,20 +81,22 @@ function clearTable(table) {
 
 //var selectedFigure = new Figure();
 //var selectedFeild = new Feild();
-
+function putFigure(cell, figure) {
+    alert("putFigure");
+    alert("putFigure befor (figure != 0)");
+        var button = document.createElement("input");
+        button.setAttribute("type","button");
+//        button.style.backgroundColor = figure.getFigureColor();
+        cell.appendChild(button);
+    alert("putFigure end");
+}
 function printBoard() {
-    alert("printBoard");
-//    this.clearTable(table);
     var table = this.mTable;
+    this.clearTable(table);
     var tableBody = document.createElement("tbody");
     boardSize = this.mBoardSize;
-    alert("printBoard befor for");
     for(var i = 0; i < boardSize; ++i) {
         var rowElement = document.createElement("tr");
-        rowElement.onclick = function() {
-            //selectedFeild.setI(this.rowIndex);
-            alert(this.rowIndex);
-        }
         for(var j = 0; j < boardSize; ++j) {
             var columnElement = document.createElement("td");
             if((i+j) % 2 == 0) {
@@ -111,14 +110,17 @@ function printBoard() {
             columnElement.setAttribute("id",id);
             columnElement.onclick = function() {
                 alert(this.cellIndex);
+                alert(this.parentNode.rowIndex);
             };
+            if(this.mGameBoard[i][j] != 0) {
+                this.putFigure(columnElement,this.mGameBoard[i][j]);
+                alert("There is a figure.");
+            }
             rowElement.appendChild(columnElement);
-            tableBody.insertRow(-1);
+            tableBody.appendChild(rowElement);
         }
-        table.appendChild(tableBody);
-        
     }
-    alert("end printBoard");
+    table.appendChild(tableBody);
 }
 
 function startGame() {
