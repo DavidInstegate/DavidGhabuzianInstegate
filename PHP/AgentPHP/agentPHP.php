6bad98f283f2904c7da21a3fca35c8fd1794a7f4
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-05-30 18:35:57 +0400

    Author: David Ghabuzian --Change PHP/AgentPHP/agentPHP.php

--- PHP/AgentPHP/agentPHP.php
+++ PHP/AgentPHP/agentPHP.php
@@ -1,25 +1,33 @@
 <html>
 <body>
 <?php
-$user = getenv( "HTTP_USER_AGENT" );
-$browser = "An unidentified browsero";
-$browsers = array('firefox','opera', 'chrome', 'safari', 'mozilla', 'netscape');
-foreach($browsers as $current) {
-    if(preg_match($current,$user)) {
-        $browser = $current;
-        break;
+class Viewer {
+    var $mUnknownType = "mUnknownType";
+    var $mBrowsers = array("Firefox","Opera","Chrome","Safari","Mozilla","Netscape","Navigator");
+    var $mOperatingSystems = array("Window","Linux");
+    function getBrowser($viewer) {
+        foreach($this->mBrowsers as $current) {
+            if(preg_match("/($current)/i", $viewer)) {
+                return $current;
+            }
+        }
+        return $this->mUnknownType;
+    }
+
+    function getOperatingSystem($viewer) {
+        foreach($this->mOperatingSystems as $current) {
+            if(preg_match("/($current)/i",$viewer)) {
+                return $current;
+            }
+        }
+        return $this->mUnknownType;
     }
 }
-$platform = "An unidentified OS!";
-if( preg_match( "/Windows/i", "$viewer" ) )
-{
-    $platform = "Windows!";
-}
-else if ( preg_match( "/Linux/i", "$viewer" ) )
-{
-    $platform = "Linux!";
-}
-echo("You are using $browser on $platform");
+$user = $_SERVER["HTTP_USER_AGENT"];
+$viewer = new Viewer();
+$browser = $viewer->getBrowser($user);
+$platform = $viewer->getOperatingSystem($user);
+echo("You are using  $browser on $platform");
 ?>
 </body>
 </html>
