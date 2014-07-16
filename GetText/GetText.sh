e0bba1b353a629503f45c3d646bb7bb9d670ed29
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-07-17 01:52:15 +0400

    Author: David Ghabuzian  - wrote a script which get text that is included int <> charecters and    prints the result into result.txt

--- GetText/GetText.sh
+++ GetText/GetText.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-text= awk '$0=$2' FS=\< RS=\> $1
-echo $text
+awk '$0=$2' FS=\< RS=\> $1&>'temp.txt'
+sort -u 'temp.txt'>'temp1.txt'
+if [ -f $2 ]; then
+    rm $2
+fi
+cat 'temp1.txt' |
+while read line
+do
+    echo '<'$line'>'>>$2
+done
+rm temp*
