a317b58a4e2fba93d5ddf4bb000791ced17c5d85
David Ghabuzian <bhabuzian@Ghabuzian-desktop.(none)>
2014-10-28 22:00:01 +0400

    auto update repository

--- C++/ForAren/type.cpp
+++ C++/ForAren/type.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 #include<typeinfo>  
 
-template <typename F, typename S> double Sum (F first, S second) {
-    double sum = 0;
-   std::cout<<typeid(first).name()<<std::endl
-       <<typeid(second).name()<<std::endl;
-    return sum;
-}
+template <typename F, typename S> double Sum (F first, S second);
+
 int main() {
-    Sum('5',5);
+    std::cout<<Sum(5,5.5);
     return 0;
+}
+
+template <typename F, typename S> double Sum (F first, S second) {
+    char type_first = *typeid(first).name();
+    char type_second = *typeid(second).name();
+    if (type_first == 'c' || type_second == 'c') {
+        std::cout<<"One or more parameters are type of char";
+        return 0;
+    }
+    return first + second;
 }
